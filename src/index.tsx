2818d90171cfa321836b4db58ab32c8e8ccb9e0f
Armen <armen.aivazian@amo.tech>
2022-05-27 21:29:26 +0300

    add react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
-import theme from './theme';
+import theme from './style/theme';
+import { BrowserRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement!);
@@ -12,6 +12,8 @@ root.render(
   <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ThemeProvider>,
 );
