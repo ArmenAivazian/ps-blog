fe68054a13519c253f04cf07dfc73bfd698d2e32
Armen <armen.aivazian@amo.tech>
2022-06-02 20:44:00 +0300

    add typist

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,7 +1,13 @@
+import Typist from 'react-typist';
+
 import Box from '@mui/material/Box';
 
 const Main = () => {
-  return <Box sx={{ height: 2000 }}></Box>;
+  return (
+    <Box sx={{ height: 2000, pt: 50 }}>
+      <Typist>Animate this text.</Typist>
+    </Box>
+  );
 };
 
 export default Main;
