346d544ab9f18bd4b0079145270d71692c23cb99
Armen <armen.aivazian@amo.tech>
2022-06-02 20:35:07 +0300

    little refactoring

--- src/i18n.js
+++ src/i18n.js
@@ -16,11 +16,13 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     fallbackLng: 'uk',
-    // debug: true,
+    debug: true,
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
+
+    supportedLngs: ['uk', 'en'],
   });
 
 export default i18n;
