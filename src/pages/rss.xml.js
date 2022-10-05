81b99c6ec79dc081c715b37e2ff149bc08836bac
Armen <armen.aivazian@amo.tech>
2022-10-05 20:48:42 +0300

    refactoring

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,9 +1,8 @@
 import rss from '@astrojs/rss';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
+import { SITE_DESCRIPTION } from '../config';
 
 export const get = () =>
 	rss({
-		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: import.meta.env.SITE,
 		items: import.meta.glob('./blog/**/*.{md,mdx}'),
